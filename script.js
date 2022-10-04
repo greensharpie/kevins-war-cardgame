daad1ca3699b175162e8eb1b48054473aa479739
Kevin Blanco <kevinjblanco@gmail.com>
2022-10-04 15:39:06 -0400

    scoreCheck function working

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
 // Variables
 let playerTurn = []
 let computerTurn = []
-let userScore = 26
+let playerScore = 26
 let computerScore = 26
 const dButton = document.getElementById('drawButton')
+const pCards = document.getElementById('playerCards')
+const compCards = document.getElementById('computerCards')
+const pScores = document.getElementById('playerScore')
+const compScore = document.getElementById('computerScore')
+
+let playerCard = 0
+let computerCard = 0
 
 //Functions for Game Logic
 function randomCards() {
   const playerCard = Math.floor(Math.random() * 14 + 2)
   playerTurn.push(playerCard)
-  console.log(playerTurn)
 
   let playerHighCards = ''
   if (playerCard === 11) {
@@ -23,6 +29,8 @@ function randomCards() {
   } else {
     playerHighCards = playerCard
   }
+
+  pCards.innerText = playerHighCards
 
   const computerCard = Math.floor(Math.random() * 14 + 2)
   computerTurn.push(computerCard)
@@ -39,8 +47,46 @@ function randomCards() {
   } else {
     computerHighCards = computerCard
   }
-  console.log(computerTurn)
+  compCards.innerText = computerHighCards
+
+  scoreCheck()
 }
 
+//scores
+
+// if (playerCard === computerCard) {
+//   war()
+// } else
+function scoreCheck() {
+  console.log('playerTurn', playerTurn)
+  console.log('computerTurn', computerTurn)
+  if (playerTurn.at(-1) > computerTurn.at(-1)) {
+    playerScore++
+    computerScore--
+    console.log('player wins')
+  } else if (computerTurn.at(-1) > playerTurn.at(-1)) {
+    computerScore++
+    playerScore--
+    console.log('computer wins')
+  }
+
+  // console.log(playerScore)
+  // console.log(computerScore)
+  pScores.innerText = playerScore
+  compScore.innerText = computerScore
+}
+// function war() {
+//   if (playerTurn === computerTrun) {
+//     war()
+//   } else if (playerTurn > computerTurn) {
+//     playerScore = playerScore + 4
+//     computerScore = computerScore - 4
+//   } else if (computerTurn > playerTurn) {
+//     computerScore = computerScore + 4
+//     playerScore = playerScore - 4
+//   }
+// }
+console.log('playerScore->', playerScore)
+console.log('computerScore->', computerScore)
 //Event Listeners
 dButton.addEventListener('click', randomCards)
